5d44a58226133645f92602f1d149a869d8ab4c1f
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-09-11 01:12:12 +0600

    feat: login implemented with ofetch + tanstack query

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -23,13 +23,16 @@ import { useForm } from "@tanstack/react-form";
 import { LoginSchema } from "@/validation";
 import React from "react";
 import { Eye, EyeClosed } from "lucide-react";
+import { useLogin } from "@/hooks";
+import { useRouter } from "next/navigation";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-
-    const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
+  const router = useRouter();
+  const { mutate: login, isPending: loginPending } = useLogin();
   const form = useForm({
     defaultValues: {
       email: "",
@@ -39,7 +42,20 @@ export function LoginForm({
       onSubmit: LoginSchema,
     },
     onSubmit: ({ value }) => {
-      console.log(value);
+      const loginData = {
+        email: value.email,
+        password: value.password,
+      };
+
+      login(loginData, {
+        onSuccess: (res) => {
+          console.log(res);
+          router.push("/");
+        },
+        onError: (err) => {
+          console.log(err);
+        },
+      });
     },
   });
 
@@ -101,26 +117,30 @@ export function LoginForm({
                         </Link>
                       </div>
                       <div className="relative">
-                      <Input
-                        onChange={(e) => field.handleChange(e.target.value)}
-                        id={field.name}
-                        type={showPassword ? "text" : "password"}
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        placeholder="••••••••"
-                        required
-                        autoComplete="off"
-                        aria-invalid={isInvalid}
-                      />
-                      <button
-                      className="absolute right-2 top-1/2 -translate-y-1/2 rounded p-1
+                        <Input
+                          onChange={(e) => field.handleChange(e.target.value)}
+                          id={field.name}
+                          type={showPassword ? "text" : "password"}
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          placeholder="••••••••"
+                          required
+                          autoComplete="off"
+                          aria-invalid={isInvalid}
+                        />
+                        <button
+                          className="absolute right-2 top-1/2 -translate-y-1/2 rounded p-1
                        text-gray-400 transition hover:text-gray-600 focus:outline-none
                        disabled:cursor-not-allowed disabled:opacity-50"
-                        type="button"
-                        onClick={() => setShowPassword(!showPassword)}
-                      >
-                        {showPassword ? <EyeClosed className="size-4" /> : <Eye  className="size-4"/>}
-                      </button>
+                          type="button"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          {showPassword ? (
+                            <EyeClosed className="size-4" />
+                          ) : (
+                            <Eye className="size-4" />
+                          )}
+                        </button>
                       </div>
                       {isInvalid && (
                         <FieldError errors={field.state.meta.errors} />
--- src/components/layouts/public/Header.tsx
+++ src/components/layouts/public/Header.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/assets/svg/Logo";
 import Link from "next/link";
 import React from "react";
 
@@ -8,12 +9,23 @@ const Header = () => {
     { name: "Contact", path: "/contact" },
   ];
   return (
-    <header className="w-full h-16 flex items-center justify-center gap-4 border-b">
-      {routes.map((route) => (
-        <Link href={route.path} key={route.path}>
-          {route.name}
-        </Link>
-      ))}
+    <header className="w-full h-16  border-b">
+      <div className="max-w-7xl mx-auto flex items-center justify-between gap-4 h-full ">
+        <h1 className="text-lg flex items-center  font-bold">
+        <Logo />
+        HealthCare Service
+      </h1>
+      <div className="flex gap-4 items-center justify-center">
+        {routes.map((route) => (
+          <Link href={route.path} key={route.path}>
+            {route.name}
+          </Link>
+        ))}
+      </div>
+      <div>
+        <Link href="/login">Login</Link>
+      </div>
+      </div>
     </header>
   );
 };
