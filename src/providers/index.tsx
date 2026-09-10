a3e106507692daa4f8131864eb271b3d8acb59c5
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-09-10 15:46:00 +0600

    feat:query provider intialized

---
+++ src/providers/index.tsx
@@ -0,0 +1,6 @@
+ 
+import QueryProvider from "./query.provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <QueryProvider>{children}</QueryProvider>;
+}
