6b47e499eed03ca05439bf18cd92e186fc0bcc91
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-09-10 17:36:12 +0600

    feat: form refactor and polished

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -21,11 +21,15 @@ import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useForm } from "@tanstack/react-form";
 import { LoginSchema } from "@/validation";
+import React from "react";
+import { Eye, EyeClosed } from "lucide-react";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+
+    const [showPassword, setShowPassword] = React.useState(false);
   const form = useForm({
     defaultValues: {
       email: "",
@@ -61,7 +65,7 @@ export function LoginForm({
                   const isInvalid =
                     field.state.meta.isTouched && !field.state.meta.isValid;
                   return (
-                    <Field>
+                    <Field data-invalid={isInvalid}>
                       <FieldLabel htmlFor="email">Email</FieldLabel>
                       <Input
                         onChange={(e) => field.handleChange(e.target.value)}
@@ -72,6 +76,7 @@ export function LoginForm({
                         placeholder="m@example.com"
                         required
                         autoComplete="off"
+                        aria-invalid={isInvalid}
                       />
                       {isInvalid && (
                         <FieldError errors={field.state.meta.errors} />
@@ -85,7 +90,7 @@ export function LoginForm({
                   const isInvalid =
                     field.state.meta.isTouched && !field.state.meta.isValid;
                   return (
-                    <Field>
+                    <Field data-invalid={isInvalid}>
                       <div className="flex items-center">
                         <FieldLabel htmlFor="password">Password</FieldLabel>
                         <Link
@@ -95,16 +100,28 @@ export function LoginForm({
                           Forgot your password?
                         </Link>
                       </div>
+                      <div className="relative">
                       <Input
                         onChange={(e) => field.handleChange(e.target.value)}
                         id={field.name}
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         value={field.state.value}
                         onBlur={field.handleBlur}
                         placeholder="••••••••"
                         required
                         autoComplete="off"
+                        aria-invalid={isInvalid}
                       />
+                      <button
+                      className="absolute right-2 top-1/2 -translate-y-1/2 rounded p-1
+                       text-gray-400 transition hover:text-gray-600 focus:outline-none
+                       disabled:cursor-not-allowed disabled:opacity-50"
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? <EyeClosed className="size-4" /> : <Eye  className="size-4"/>}
+                      </button>
+                      </div>
                       {isInvalid && (
                         <FieldError errors={field.state.meta.errors} />
                       )}
